66e31fdd9f86f3595b797db7843d798293d08e34
esrasumer <esrasumer28@gmail.com>
2022-06-06 21:56:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+ // İf/Else Statements
+
+ const firstName = 'Sena';
+ const age = 29;
+ const isStudent = false;
+ const school = 'univercity';
+ const id = 12345678;
+
+ /** 
+ if (firstName === 'Sena') {
+     console.log('Your name is Sena.');
+ } else {
+     console.log('You are not Sena.');
+ }
+
+ if (age === 19) {
+     console.log('Your age is 19.');
+ } else {
+     console.log('you are not');
+ }
+
+ if (isStudent === true) {
+     console.log('You are a student.');
+ } else {
+     console.log('You are not a student.');
+ }
+
+ if (school === 'Univercity') {
+     console.log('Congradulations! You are in the Univercity.');
+ } else {
+     console.log('You are a loser. ');
+ }
+
+
+ if (age >= 18) {
+
+ } else {
+     console.log('ehliyet alamazsınız.');
+ }
+
+ if (school == 'univercity') {
+     console.log('ehliyet alabilirsiniz.');
+ } else {
+     console.log('eğitim durumunuz yetersiz.');
+ } 
+
+
+ // && ve   || veya  
+
+ if (age > 0 && age < 12) {
+     console.log(`${firstName} is a child `);
+ } else if (age >= 13 && age <= 19) {
+     console.log(`${firstName} is a teenager`);
+ } else {
+     console.log(`${firstName} is an adult`);
+ }
+  */
